da308e75927346f323d637953bd427af2365a147
pericarney00 <pericarney@gmail.com>
2025-09-08 16:21:13 -0500

    Created and rendered route for games index. wrote mock data for the index

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+class Game:
+  def __init__(self, name, genre, description, console):
+    self.name = name
+    self.genre = genre
+    self.description = description
+    self.console = console
+
+games = [
+  Game('Stardew Valley', 'farming-simulator','low-graphics, fun to play, theres no right way to play','Nintendo Switch, PC'),
+  Game('Minecraft', 'survival, sandbox','low-graphics, fun to play, theres no right way to play','Nintendo Switch, PC'),
+  Game('Fortnite', 'first-perspon shooter','good graphics,multi-player','Nintendo Switch'),
+]
+
 # Create your views here.
 
 
@@ -7,4 +22,7 @@ def home(request):
   return HttpResponse('<h1> howdy </h1>')
 
 def about(request):
-  return HttpResponse('<h1> About GameStash </h1>')
+  return render(request, 'about.html')
+
+def games_index(request):
+  return render(request, 'games/index.html', {'games': games})
