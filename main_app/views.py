8a846d4080f79e8abd6a6a981e65033e9ec9b552
pericarney00 <pericarney@gmail.com>
2025-09-09 14:16:24 -0500

    Created and merged game model

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-class Game:
-  def __init__(self, name, genre, description, console):
-    self.name = name
-    self.genre = genre
-    self.description = description
-    self.console = console
-
-games = [
-  Game('Stardew Valley', 'farming-simulator','low-graphics, fun to play, theres no right way to play','Nintendo Switch, PC'),
-  Game('Minecraft', 'survival, sandbox','low-graphics, fun to play, theres no right way to play','Nintendo Switch, PC'),
-  Game('Fortnite', 'first-perspon shooter','good graphics,multi-player','Nintendo Switch'),
-]
-
 # Create your views here.
 
 
