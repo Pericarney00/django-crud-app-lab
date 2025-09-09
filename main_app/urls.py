5ef0bd75fed7597396b27fd2029a8cf17b51a481
pericarney00 <pericarney@gmail.com>
2025-09-09 15:20:16 -0500

    Created game details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('games/', views.games_index, name='games-index'),
+  path('games/', views.game_index, name='game-index'),
+  path('games/<int:game_id>/', views.game_detail, name = 'game-detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Game
 
 # Create your views here.
 
@@ -11,5 +11,10 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def games_index(request):
+def game_index(request):
+  games = Game.objects.all()
   return render(request, 'games/index.html', {'games': games})
+
+def game_detail(request, game_id):
+  game = Game.objects.get(id=game_id)
+  return render(request, 'games/detail.html', {'game':game})
