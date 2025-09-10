ee8eff6021f133efd639f97b0d62168ad7d81315
pericarney00 <pericarney@gmail.com>
2025-09-10 14:43:04 -0500

    Rendered home html

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,7 @@ from .models import Game
 
 
 def home(request):
-  return HttpResponse('<h1> howdy </h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
